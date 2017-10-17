7b0351856ac68fdc524a4f9a74b4ad18c6d0b1fb
RefillAbuser1 <32843024+RefillAbuser1@users.noreply.github.com>
2017-10-17 06:17:10 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,16 +6,27 @@ const config = require("./config.json");
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 
-bot.user.setPresence({game: {name: 'prefix: @help', type:0 } });
+bot.user.setPresence({game: {name: 'NeverLand | Maintance', type:0 } });
 });
 
 bot.on('message', message => {
   if(message.author.bot) return;
   if(!message.content.startsWith(config.prefix)) return;
-  }
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(config.prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command === "add") {
+    let numArry = args.map(n=> parseInt(n));
+    let total = numArry.reduce( (p, c) => p+c);
+
+    message.channel.sendMessage(total);
+  } 
   
   if (command === "help") {
-	 message.channel.sendMessage("NeverLand bot in Maintance");
+	 message.channel.sendMessage("Commands: @add, @help");
   }
   
 });
